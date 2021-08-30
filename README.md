157dfa23a56117bcfc03b4b4c25360628aba4964
paulfranzen <paulfranzen@gmail.com>
2021-08-30 14:07:39 -0500

    set u-p first ttwo projects

---
+++ README.md
@@ -0,0 +1,9 @@
+# README, PLEASE
+
+### Three Challenges
+
+Rather than an hours long, needlessly stressful live coding interview at a white board, we request you complete the these three challenges.
+
+1. Simple html and css layout
+2. Simple ruby sorting
+3. 
---
+++ sort/script.rb
@@ -0,0 +1,105 @@
+episodes = [
+    {
+      title: "Bart of Darkness",
+      main: "Bart"
+    },
+    {
+      title: "Lisa’s Substitute",
+      main: 'Lisa'
+    },
+    {
+      title: "Treehouse of Horror 1",
+      main: 'Bart'
+    },
+    {
+      title: "Lisa’s Wedding",
+      main: 'Lisa'
+    },
+    {
+      title: "Santa's little helper",
+      main: 'Lisa'
+    },
+    {
+      title: "Dancing Homer",
+      main: 'Homer'
+    },
+    {
+      title: "Lisa’s Pony",
+      main: 'Lisa'
+    },
+    {
+      title: "Homer The Smithers",
+      main: 'Homer'
+    },
+    {
+      title: "Homie The Clown",
+      main: 'Homer'
+    },
+    {
+      title: "D'oh-In' In The Wind",
+      main: 'Bart'
+    },
+    {
+      title: "Lisa Vs. Malibu Stacy",
+      main: 'Lisa'
+    },
+    {
+      title: "Homer’s Barbershop Quartet",
+      main: "Homer"
+    },
+    {
+      title: "Lisa’s Date With Destiny",
+      main: 'Lisa'
+    },
+    {
+      title: "Barts comet",
+      main: 'Bart'
+    },
+    {
+      title: "Homer’s Phobia",
+      main: 'Homer'
+    },
+    {
+      title: "Fear Of Flying",
+      main: 'Marge'
+    },
+    {
+      title: "The Springfield Connection",
+      main: 'Marge'
+    },
+    {
+      title: "Marge vs. The Monorail",
+      main: 'Marge'
+    },
+    {
+      title: "A Streetcar Named Marge",
+      main: 'Marge'
+    },
+    {
+      title: "Moaning Lisa",
+      main: 'Lisa'
+    },
+    {
+      title: "Lisa The Beauty Queen",
+      main: 'Lisa'
+    },
+    {
+      title: "The Secret War Of Lisa Simpson",
+      main: 'Lisa'
+    },
+    {
+      title: "Homer Vs. Lisa And The 8th Commandment",
+      main: "Homer"
+    }
+  ]
+
+
+puts 'Write a simple ruby script to reformat the data to:  
+{
+  bart: {
+    number_of_episodes: [INT]
+  },
+  homer: {
+    number_of_episodes: [INT]
+  }
+} etc...'
